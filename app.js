dfdee47629b18b01beb3bb42cf0f787ab395baa6
Pedro <156568407+pedrocafern@users.noreply.github.com>
2025-04-25 14:25:53 -0300

    app.js

--- app.js
+++ app.js
@@ -6,10 +6,23 @@
 const Numero = Math.floor(Math.random() * 10) + 1;
 let tent = 1;
 
+function exibirNaTela(tag, texto) {
+  let campo = document.querySelector(tag);
+  campo.innerHTML = texto;
+}
+
+function mensagemNaTela() {
+  exibirNaTela('h1', 'Math Game')
+  exibirNaTela('p', 'Escolha um número de 1 a 10:"
+}
+
+mensagemNaTela();
+
 function jogar() {
   const entrada = document.getElementById('num');
   const msg = document.getElementById('msg');
   const bismo = +entrada.value;
+
   
 
   if (bismo < 1 || bismo > 10) return msg.textContent = 'Não entendeu como joga?';
